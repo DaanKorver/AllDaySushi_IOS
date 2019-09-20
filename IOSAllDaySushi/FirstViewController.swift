f5f30b23b91e226ce2711d569a36097b1e1ae3f4
DaanKorver <daank2000@hotmail.nl>
2019-09-20 12:04:27 +0200

    Initial Commit

---
+++ IOSAllDaySushi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  IOSAllDaySushi
+//
+//  Created by Student on 20/09/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOSAllDaySushi/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  IOSAllDaySushi
+//
+//  Created by Student on 20/09/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
